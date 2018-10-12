b2d41f9493e3b49ef4a365766b9cfaffab9a4aeb
Colton McCurdy <mccurdyc22@gmail.com>
2018-10-12 19:02:47 -0400

    update setup script to copy sample config to execution config

--- build/setup.sh
+++ build/setup.sh
@@ -4,6 +4,15 @@ GOBAK="$SYS_GOCMD.bak"
 
 NEIGHBOR_DIR=$(pwd)
 NEIGHBOR_GOCMD="$NEIGHBOR_DIR/bin/go-cover"
+NEIGHBOR_CFG="$NEIGHBOR_DIR/config.json"
+NEIGHBOR_SAMPLE_CFG="$NEIGHBOR_DIR/sample.config.json"
+
+if [ -f $NEIGHBOR_CFG ]; then
+  echo "neighbor config already exists ($NEIGHBOR_CFG), doing nothing."
+else
+  echo "Creating a neighbor config file ($NEIGHBOR_CFG) from the sample config."
+	cp $NEIGHBOR_SAMPLE_CFG $NEIGHBOR_CFG
+fi
 
 # will need to be run as `root`
 if [ -f $GOBAK ]; then
