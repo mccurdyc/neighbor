d73b4853b17c5694363811cc8d7e294df9a52664
Colton McCurdy <mccurdyc22@gmail.com>
2018-10-13 16:30:22 -0400

    set COVERPROFILE_OUT_PATH for each project before running test command

--- pkg/external/test.go
+++ pkg/external/test.go
@@ -2,6 +2,7 @@ package external
 
 import (
 	// stdlib
+	"fmt"
 	"os"
 
 	// external
@@ -14,7 +15,17 @@ import (
 func RunTests(ctx *neighbor.Ctx) {
 	for name, dir := range ctx.ProjectDirMap {
 		ctx.Logger.Infof("running tests for %s", name)
+
 		err := os.Chdir(dir)
+		if err != nil {
+			ctx.Logger.Error(err)
+			continue
+		}
+
+		cp := fmt.Sprintf("%s/neighbor-%s-coverprofile.out", dir, name)
+
+		err = os.Setenv("COVERPROFILE_OUT_PATH", cp)
+		ctx.Logger.Infof("setting COVERPROFILE_OUT_PATH for %s to (%s)", name, cp)
 		if err != nil {
 			ctx.Logger.Error(err)
 			continue
