67822cad63d9ec251327268cb497b680aac78b3c
Colton McCurdy <mccurdyc22@gmail.com>
2018-11-16 08:45:38 -0500

    fix parsing of external command to consider commands with " in them
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

--- pkg/neighbor/cmd.go
+++ pkg/neighbor/cmd.go
@@ -1,22 +1,60 @@
 package neighbor
 
 import (
+	// stdlib
+
+	"errors"
 	"strings"
+	"unicode"
+
+	// external
+	"golang.org/x/text/unicode/rangetable"
+	// internal
 )
-
-// stdlib
-
-// external
-// internal
 
 // SetExternalCmd sets the command that will be run on external projects.
 func (c *Ctx) SetExternalCmd(s string) error {
-	cmd := parseCmd(s)
+	cmd, err := parseCmd(s)
+	if err != nil {
+		return err
+	}
 
 	c.ExternalCmd = cmd
 	return nil
 }
 
-func parseCmd(s string) []string {
-	return strings.Split(s, " ")
+// parseCmd ensures that the entered command is valid, then splits on ".
+// Then, iterates over the elements, where odd-indexed elements are those surrounding
+// the ". Even-indexed elements are those surrounded by ".
+func parseCmd(s string) ([]string, error) {
+	var res []string
+
+	if !valid(s) {
+		return res, errors.New("invalid external command") // @TODO add more descriptive error
+	}
+
+	spl := strings.FieldsFunc(s, func(r rune) bool {
+		// string contains "
+		// https://en.wikipedia.org/wiki/List_of_Unicode_characters
+		return unicode.In(r, rangetable.New(34))
+	})
+
+	for i, v := range spl {
+		if i%2 != 0 {
+			res = append(res, strings.Split(v, " ")...)
+		} else {
+			res = append(res, v)
+		}
+	}
+
+	return res, nil
+}
+
+func valid(s string) bool {
+	// if string starts with \", invalid command
+	if strings.HasPrefix(s, "\"") {
+		return false
+	}
+
+	return true
 }
