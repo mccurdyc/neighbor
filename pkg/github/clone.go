9e844a02cd60c792c0cb69f4d4325936bc810d20
Colton McCurdy <mccurdyc22@gmail.com>
2019-12-29 18:10:41 -0500

    pkg/github: clean up Clone API
    * remove unnecessary withAuth and just explicitly set

--- pkg/github/clone.go
+++ pkg/github/clone.go
@@ -14,39 +14,49 @@ type Cloner interface {
 	Clone(context.Context, string, github.Repository)
 }
 
-func WithCloneURL(opts git.CloneOptions, url string) git.CloneOptions {
-	opts.URL = url
-	return opts
+type CloneConfig struct {
+	URL  string
+	Auth transport.AuthMethod
 }
 
-func WithTokenAuth(opts git.CloneOptions, token string) git.CloneOptions {
-	opts.Auth = withAuth(&http.BasicAuth{
+func NewCloneConfig(repo *github.Repository) CloneConfig {
+	return CloneConfig{
+		URL: repo.GetCloneURL(),
+	}
+}
+
+func (cf CloneConfig) WithTokenAuth(token string) CloneConfig {
+	cf.Auth = &http.BasicAuth{
 		Username: "null",
 		Password: token,
-	})
-	return opts
+	}
+	return cf
 }
 
-func WithBasicAuth(opts git.CloneOptions, username, password string) git.CloneOptions {
-	opts.Auth = withAuth(&http.BasicAuth{
+func (cf CloneConfig) WithBasicAuth(username, password string) CloneConfig {
+	cf.Auth = &http.BasicAuth{
 		Username: username,
 		Password: password,
-	})
-	return opts
+	}
+	return cf
 }
 
-func WithAuth(opts git.CloneOptions, auth transport.AuthMethod) git.CloneOptions {
-	opts.Auth = withAuth(auth)
-	return opts
-}
-
-func withAuth(auth transport.AuthMethod) transport.AuthMethod {
-	return auth
+func (cf CloneConfig) WithAuth(auth transport.AuthMethod) CloneConfig {
+	cf.Auth = auth
+	return cf
 }
 
 // Clone clones a project to the directory specified by dir with the project directory
 // name being the 'username/repository'. Note that repository is not a sub-directory.
-func Clone(ctx context.Context, dir string, repo github.Repository, opts git.CloneOptions) error {
+func Clone(ctx context.Context, dir string, repo github.Repository, cfg CloneConfig) error {
+	opts := git.CloneOptions{
+		URL: cfg.URL,
+	}
+
+	if cfg.Auth != nil {
+		opts.Auth = cfg.Auth
+	}
+
 	err := opts.Validate()
 	if err != nil {
 		return fmt.Errorf("CloneOptions are invalid: %w", err)
@@ -54,7 +64,7 @@ func Clone(ctx context.Context, dir string, repo github.Repository, opts git.Clo
 
 	_, err = git.PlainClone(dir, false, &opts)
 	if err != nil {
-		return fmt.Errorf("failed to clone project %s to %s: %w", *repo.Name, dir, err)
+		return fmt.Errorf("failed to clone '%s' to '%s/': %w", *repo.Name, dir, err)
 	}
 	return nil
 }
