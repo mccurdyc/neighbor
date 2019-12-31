84db8d6344f3943b9cd734e898944dbb6cc006fe
Colton McCurdy <mccurdyc22@gmail.com>
2019-12-31 13:29:09 -0500

    pkg/github: add GoDoc comments to clone
    * unexport CloneConfig.auth and CloneConfig.url fields

--- pkg/github/clone.go
+++ pkg/github/clone.go
@@ -14,57 +14,79 @@ import (
 
 const numWorkers = 5
 
+// Cloner is the interface required to clone a repository.
 type Cloner interface {
 	Clone(context.Context, string, github.Repository)
 }
 
+// CloneConfig is used to configure cloning.
 type CloneConfig struct {
-	URL  string
-	Auth transport.AuthMethod
+	url  string
+	auth transport.AuthMethod
 }
 
-func NewCloneConfig(repo *github.Repository) CloneConfig {
-	return CloneConfig{
-		URL: repo.GetCloneURL(),
-	}
+// NewCloneConfig creates a new, empty, CloneConfig.
+func NewCloneConfig() CloneConfig {
+	return CloneConfig{}
 }
 
+// WithRepoURL returns a new CloneConfig with the url field set to the repository's
+// CloneURL.
+func (cf CloneConfig) WithRepoURL(repo *github.Repository) CloneConfig {
+	cf.url = repo.GetCloneURL()
+	return cf
+}
+
+// WithTokenAuth is http.BasicAuth configured so that an API token can be used
+// instead of a username and password. This is how GitHub handles token authentication
+// https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line#using-a-token-on-the-command-line
 func (cf CloneConfig) WithTokenAuth(token string) CloneConfig {
-	cf.Auth = &http.BasicAuth{
+	cf.auth = &http.BasicAuth{
 		Username: "null",
 		Password: token,
 	}
 	return cf
 }
 
+// WithBasicAuth is authentication via username and password.
 func (cf CloneConfig) WithBasicAuth(username, password string) CloneConfig {
-	cf.Auth = &http.BasicAuth{
+	cf.auth = &http.BasicAuth{
 		Username: username,
 		Password: password,
 	}
 	return cf
 }
 
+// WithAuth sets the auth method to an arbitrary auth method that satisfies the
+// transport.AuthMethod interface, specified by the consumer.
 func (cf CloneConfig) WithAuth(auth transport.AuthMethod) CloneConfig {
-	cf.Auth = auth
+	cf.auth = auth
 	return cf
 }
 
+// ErrorWithMeta contains the error, or nil, incurred while cloning the repository
+// with additional metadata about the cloned repository.
 type ErrorWithMeta struct {
 	Error error
 
-	ErrorMeta
+	Meta
 }
 
-type ErrorMeta struct {
+// Meta contains information about the cloned repository.
+type Meta struct {
+	// RepositoryName is the full repository name i.e., 'username/reponame'.
 	RepositoryName string
+	// ClonedDir is the filepath to the cloned repository.
+	ClonedDir string
 }
 
 // CloneRepositories clones repositories as quickly as possible as subdirectories
 // where the subdirectory name is the repository name, with the parent directory
 // specified by dir. CloneRepositories guarantees that the repositories will be
-// cloned as long as an error is not incurred.
-func CloneRepositories(ctx context.Context, dir string, repos []*github.Repository, errCh chan ErrorWithMeta, cfg CloneConfig) {
+// cloned as long as an error is not incurred. Errors incurred cloning a repository
+// will not prevent attempts at cloning additional repositories. doneCh will
+// be populated with the error value, or nil otherwise for each repository.
+func CloneRepositories(ctx context.Context, dir string, repos []*github.Repository, doneCh chan ErrorWithMeta, cfg CloneConfig) {
 	ch := make(chan github.Repository)
 
 	var wg sync.WaitGroup
