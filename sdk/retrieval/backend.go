045a1373fb5ee44eed86c8d3cf6c7f9179323fc4
Colton McCurdy <mccurdyc22@gmail.com>
2020-01-02 06:59:24 -0500

    sdk/retrieval - define retrieval factory
    * still need to add tests

---
+++ sdk/retrieval/backend.go
@@ -0,0 +1,21 @@
+package retrieval
+
+import "context"
+
+// Backend is the minimal interface that must be implemented by a retriever.
+// Retrievers are responsible for obtaining projects, the base entity on which
+// neighbor operates. An example retriever is Git.
+type Backend interface {
+	Retrieve(context.Context, string, string) error
+}
+
+type BackendConfig struct {
+	// AuthRequired is indicates the method, if one, to be used for authentication
+	// by the retrieval backend.
+	AuthMethod string
+
+	Config map[string]string
+}
+
+// Factory is a factory function for constructing retrievers backend.
+type Factory func(context.Context, *BackendConfig) (Backend, error)
