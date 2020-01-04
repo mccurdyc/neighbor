66425a29acd35035c6de4851c89eb81cde8ffd1c
Colton McCurdy <mccurdyc22@gmail.com>
2020-01-04 16:26:19 -0500

    pkg/github/search: migrating code to GitHub search

--- pkg/github/search.go
+++ pkg/github/search.go
@@ -6,94 +6,6 @@ import (
 
 	"github.com/google/go-github/github"
 )
-
-// maxPageSize is the max number of results per page that GitHub returns.
-// https://developer.github.com/v3/#pagination
-const maxPageSize = 100
-
-type searchOptions struct {
-	numDesiredResults int
-	maxPageSize       int
-
-	gitHubSearchOptions github.SearchOptions
-}
-
-// SearchOptions returns the default searchOptions values (e.g., by default neighbor
-// will attempt to return 100 repositories or one full page of results).
-func SearchOptions() searchOptions {
-	return searchOptions{
-		numDesiredResults:   maxPageSize,
-		maxPageSize:         maxPageSize,
-		gitHubSearchOptions: github.SearchOptions{},
-	}
-}
-
-// WithNumberOfResults sets the number of desired results that should be obtained.
-func (so searchOptions) WithNumberOfResults(n int) searchOptions {
-	so.numDesiredResults = n
-	return so
-}
-
-// WithGitHubOptions sets the optional search parameters specified by GitHub.
-// https://godoc.org/github.com/google/go-github/github#SearchOptions
-func (so searchOptions) WithGitHubOptions(opts github.SearchOptions) searchOptions {
-	so.gitHubSearchOptions = opts
-	return so
-}
-
-// SearchType defines the supported search types.
-// https://developer.github.com/v3/search/
-//
-// GitHub supports the following search types:
-//  + Repository
-//  + Code
-//  + Commits
-//  + Issues and Pull Request
-//  + Users
-//  + Topics
-//  + Labels
-//  + Text Match Metadata
-type SearchType string
-
-const (
-	Repository SearchType = "repository"
-	Code       SearchType = "code"
-)
-
-// Results contains the full list of repositories --- i.e., from multiple pages ---
-// returned from a search, the response from GitHub which indicates how many total
-// results and pagination information and the rawSearchResults which contains
-// the search result information in addition to the repositories.
-type Results struct {
-	Repositories     []*github.Repository
-	response         *github.Response
-	rawSearchResults interface{}
-}
-
-// Searcher is an interface for searching and processing results.
-type Searcher interface {
-	search(context.Context, string, *github.SearchOptions) (Results, error)
-
-	ResultProcessor
-}
-
-// ResultProcessor is an interface for processing the raw results from a search.
-type ResultProcessor interface {
-	processResults(interface{}, *github.Response) Results
-}
-
-// NewSearcher creates a new Searcher or indicates when a search type is not supported.
-func NewSearcher(c *github.Client, t SearchType) (Searcher, error) {
-	switch t {
-	case Repository:
-		return NewRepositorySearcher(c), nil
-	case Code:
-		return NewCodeSearcher(c), nil
-	default:
-		return nil, errors.New("unsupported search type")
-	}
-}
-
 // ErrFewerResultsThanDesired is used to indicate that it was not possible to fulfill
 // the request from the user (i.e., could not find the number of results specified
 // by the user).
@@ -129,15 +41,4 @@ func Search(ctx context.Context, s Searcher, query string, opts searchOptions) (
 
 		opts.gitHubSearchOptions.Page = page + 1
 	}
-}
-
-// pageSize returns the minimal page size necessary to fulfill the request or the
-// maximum page supported by GitHub.
-// https://developer.github.com/v3/#pagination
-func pageSize(desired, max int) int {
-	if desired < max {
-		return desired
-	}
-
-	return max
 }
