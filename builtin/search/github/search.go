42cfc39c91ae76260697c8e51d629011d55209cf
Colton McCurdy <mccurdyc22@gmail.com>
2020-01-04 18:20:46 -0500

    builtin/search/github: WIP rough first pass

--- builtin/search/github/search.go
+++ builtin/search/github/search.go
@@ -1,32 +1,97 @@
 package github
 
-func searchRepository() {
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/go-github/github"
+	githubProject "github.com/mccurdyc/neighbor/builtin/project/github"
+	"github.com/mccurdyc/neighbor/sdk/project"
+)
+
+// maxPageSize is the max number of results per page that GitHub returns.
+// https://developer.github.com/v3/#pagination
+const maxPageSize = 100
+
+type searchOptions struct {
+	numDesiredResults int
+	maxPageSize       int
+}
+
+func searchRepositories(ctx context.Context, c *github.Client, query string, numDesiredResults int) ([]project.Backend, error) {
+	res := make([]project.Backend, 0, numDesiredResults)
+
+	gitHubSearchOptions := github.SearchOptions{
+		ListOptions: github.ListOptions{
+			PerPage: pageSize(numDesiredResults, maxPageSize),
+		},
+	}
+
+	searchRes, _, err := c.Search.Repositories(ctx, query, &gitHubSearchOptions)
+	if err != nil {
+		return res, err
+	}
+
+	for _, repo := range searchRes.Repositories {
+		var version string
+		latest, _ := getLatestCommit(ctx, c, repo)
+		if latest != nil {
+			version = latest.GetSHA()
+		}
+
+		p, err := githubProject.Factory(ctx, &project.BackendConfig{
+			Name:           repo.GetFullName(),
+			Version:        version,
+			SourceLocation: repo.GetCloneURL(),
+		})
+		if err != nil {
+			continue
+		}
+
+		res = append(res, p)
+	}
+
+	return res, nil
+}
+
+func getLatestCommit(ctx context.Context, c *github.Client, repo github.Repository) (*github.RepositoryCommit, error) {
+	commits, _, err := c.Repositories.ListCommits(ctx, repo.GetOwner().String(), repo.GetName(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(commits) < 1 {
+		return nil, nil
+	}
+
+	return commits[0], nil
+}
+
+func searchCode(ctx context.Context) ([]project.Backend, error) {
 	panic("not implemented")
 }
 
-func searchCode() {
-	panic("not implemented")
-}
-
-func searchMeta(entity searchMethodEntity) {
+func searchMeta(ctx context.Context, entity searchMethodEntity) ([]project.Backend, error) {
 	switch entity {
 	case topic:
-		searchTopic()
+		return searchTopic(ctx)
 	case textMatch:
-		searchTextMatch()
+		return searchTextMatch(ctx)
 	case label:
-		searchLabel()
+		return searchLabel(ctx)
 	}
+
+	return nil, fmt.Errorf("search method entity unsupported")
 }
 
-func searchTopic() {
+func searchTopic(ctx context.Context) ([]project.Backend, error) {
 	panic("not implemented")
 }
 
-func searchLabel() {
+func searchLabel(ctx context.Context) ([]project.Backend, error) {
 	panic("not implemented")
 }
 
-func searchTextMatch() {
+func searchTextMatch(ctx context.Context) ([]project.Backend, error) {
 	panic("not implemented")
 }
