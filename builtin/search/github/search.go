ab040d383e7b3e1a7986302e25f76534aaa69aa8
Colton McCurdy <mccurdyc22@gmail.com>
2020-01-08 07:55:24 -0500

    builtin/search/github: Code search
    * implement code search for obtaining repositories

--- builtin/search/github/search.go
+++ builtin/search/github/search.go
@@ -52,21 +52,41 @@ func searchRepositories(ctx context.Context, c Client, query string, numDesiredR
 	return res, resp, nil
 }
 
-func getLatestCommit(ctx context.Context, c Client, repo github.Repository) (*github.RepositoryCommit, error) {
-	commits, _, err := c.RepositoryService.ListCommits(ctx, repo.GetOwner().GetName(), repo.GetName(), nil)
+func searchCode(ctx context.Context, c Client, query string, numDesiredResults int, opts *github.SearchOptions) ([]project.Backend, *github.Response, error) {
+	res := make([]project.Backend, 0, numDesiredResults)
+
+	searchRes, resp, err := c.SearchService.Code(ctx, query, opts)
 	if err != nil {
-		return nil, err
+		return res, resp, err
 	}
 
-	if len(commits) < 1 {
-		return nil, nil
+	if searchRes == nil {
+		return res, resp, fmt.Errorf("empty code response")
 	}
 
-	return commits[0], nil
-}
+	for _, r := range searchRes.CodeResults {
+		repo := r.Repository
 
-func searchCode(ctx context.Context, opts *github.SearchOptions) ([]project.Backend, *github.Response, error) {
-	panic("not implemented")
+		var version string
+
+		latest, _ := getLatestCommit(ctx, c, *repo)
+		if latest != nil {
+			version = latest.GetSHA()
+		}
+
+		p, err := githubProject.Factory(ctx, &project.BackendConfig{
+			Name:           repo.GetFullName(),
+			Version:        version,
+			SourceLocation: repo.GetCloneURL(),
+		})
+		if err != nil {
+			continue
+		}
+
+		res = append(res, p)
+	}
+
+	return res, resp, nil
 }
 
 func searchMeta(ctx context.Context, entity searchMethodEntity, opts *github.SearchOptions) ([]project.Backend, *github.Response, error) {
@@ -92,4 +112,17 @@ func searchLabel(ctx context.Context, opts *github.SearchOptions) ([]project.Bac
 
 func searchTextMatch(ctx context.Context, opts *github.SearchOptions) ([]project.Backend, *github.Response, error) {
 	panic("not implemented")
+}
+
+func getLatestCommit(ctx context.Context, c Client, repo github.Repository) (*github.RepositoryCommit, error) {
+	commits, _, err := c.RepositoryService.ListCommits(ctx, repo.GetOwner().GetName(), repo.GetName(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(commits) < 1 {
+		return nil, nil
+	}
+
+	return commits[0], nil
 }
