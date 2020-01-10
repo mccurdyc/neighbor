860dcf3f9dc6a6cd4fd8e52cf9e4117ba3bebdef
Colton McCurdy <mccurdyc22@gmail.com>
2020-01-10 07:06:37 -0500

    builtin/search/github: test for deduplication
    * test the internal `contains` function for deduplication

--- builtin/search/github/search_test.go
+++ builtin/search/github/search_test.go
@@ -2,6 +2,9 @@ package github
 
 import (
 	"testing"
+
+	"github.com/mccurdyc/neighbor/sdk/project"
+	"github.com/mccurdyc/neighbor/sdk/retrieval"
 )
 
 type mockURLRetriever struct {
@@ -58,6 +61,74 @@ func Test_getCloneURL(t *testing.T) {
 
 			if got != tt.want.value {
 				t.Errorf("getCloneURL(%+v): \n\tgot: '%+v'\n\twant: '%+v'", tt.input, got, tt.want.value)
+			}
+		})
+	}
+}
+
+type mockProject struct {
+	name           string
+	version        string
+	retrievalFunc  retrieval.Backend
+	sourceLocation string
+	localLocation  string
+}
+
+func (m *mockProject) Name() string                     { return m.name }
+func (m *mockProject) Version() string                  { return m.version }
+func (m *mockProject) RetrievalFunc() retrieval.Backend { return m.retrievalFunc }
+func (m *mockProject) SourceLocation() string           { return m.sourceLocation }
+func (m *mockProject) LocalLocation() string            { return m.localLocation }
+func (m *mockProject) SetLocalLocation(l string) project.Backend {
+	m.localLocation = l
+	return m
+}
+
+func Test_contains(t *testing.T) {
+	type input struct {
+		projects []project.Backend
+		p        project.Backend
+	}
+
+	type want struct {
+		value bool
+	}
+
+	var tests = map[string]struct {
+		input input
+		want  want
+	}{
+		"project_contained": {
+			input: input{
+				projects: []project.Backend{
+					&mockProject{name: "one/one"},
+					&mockProject{name: "two/two"},
+					&mockProject{name: "three/three"},
+				},
+				p: &mockProject{name: "two/two"},
+			},
+			want: want{value: true},
+		},
+
+		"project_not_contained": {
+			input: input{
+				projects: []project.Backend{
+					&mockProject{name: "one/one"},
+					&mockProject{name: "two/two"},
+					&mockProject{name: "three/three"},
+				},
+				p: &mockProject{name: "four/four"},
+			},
+			want: want{value: false},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := contains(tt.input.projects, tt.input.p)
+
+			if got != tt.want.value {
+				t.Errorf("contains(%+v): \n\tgot: '%+v'\n\twant: '%+v'", tt.input, got, tt.want.value)
 			}
 		})
 	}
