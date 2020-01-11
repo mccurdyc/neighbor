ec3b7a18cebd25ac07311d62ca9e87a0152d6651
Colton McCurdy <mccurdyc22@gmail.com>
2020-01-11 07:09:35 -0500

    sdk/run: adds params to Run()
    This change adds a context.Context and dir string to the parameters
    of the `Run` function.

--- sdk/run/backend.go
+++ sdk/run/backend.go
@@ -7,17 +7,13 @@ import (
 
 // Backend is the minimal interface for a search backend.
 type Backend interface {
-	Run() error
+	Run(context.Context, string) error
 }
 
 // BackendConfig contains the configuration parameters for a search backend.
 type BackendConfig struct {
-	// Name is the name of the command to be run.
-	Name string
-	// Args are the arguments for the command, specified by Name.
-	Args string
-	// Dir is the directory that should be used as the working directory for commands.
-	Dir string
+	// Cmd is the command to be run.
+	Cmd string
 	// Stdout is where output should be written to.
 	Stdout io.Writer
 	// Stderr is where error output should be written to.
